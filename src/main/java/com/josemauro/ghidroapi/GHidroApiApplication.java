ba18bc702a26dabf1b08943c6def784df11a7fec
José Mauro <josemaurosilva600@gmail.com>
2025-10-31 20:13:56 -0300

    Criação do projeto com Spring Boot

---
+++ src/main/java/com/josemauro/ghidroapi/GHidroApiApplication.java
@@ -0,0 +1,13 @@
+package com.josemauro.ghidroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHidroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GHidroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemauro/ghidroapi/GHidroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemauro.ghidroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GHidroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
